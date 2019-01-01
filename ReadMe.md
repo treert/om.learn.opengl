18c77ff37bbbda6b115dc5eaa37b0e92a772f316
om.gx8 <909413016@qq.com>
2019-01-01 16:47:16 +0800

    init

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+## 概述
+看LearnOpenGL，学OpenGl。
+
+开始使用cmake来管理CXX的编译。
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+#include<glad/glad.h>
+#include <GLFW\glfw3.h>
+
+using namespace std;
+
+int main(){
+	cout << "start" << endl;
+	return 0;
+}
